0f07a11fd8d9d28ec98d2857a0bd22abafbf02c2
TaeWon <wkdrmsdpskfk@naver.com>
2020-04-15 16:35:52 +0900

    Add my_module [Add]

---
+++ my_module.py
@@ -0,0 +1,5 @@
+Python 3.7.1rc2 (v3.7.1rc2:6c06ef7dc3, Oct 13 2018, 15:44:37) [MSC v.1914 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+def add(a,b):
+	return a+b
